c70df2b40fca72a7cb41d395f300fb4c1a8255a4
Jake <wizecracker55@yahoo.com>
2014-01-17 23:01:12 -0600

    php extension

--- Twitter/config.php
+++ Twitter/config.php
@@ -7,4 +7,4 @@
 
 define('CONSUMER_KEY', 'TpWYB2YqY7JTJyuMosyEbA');
 define('CONSUMER_SECRET', '46aF7cHTBZ3UWrZJN0UO3vO1a673Z5th0yhQZnOYkc');
-define('OAUTH_CALLBACK', 'http://www.boothhacks.com/W2014/Twitter');
+define('OAUTH_CALLBACK', 'http://www.boothhacks.com/W2014/Twitter/index.php');
