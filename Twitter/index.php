0488280e7873fea542b9a0a3c087e574efd0b570
Jake <wizecracker55@yahoo.com>
2014-01-18 00:23:44 -0600

    a

--- Twitter/index.php
+++ Twitter/index.php
@@ -22,8 +22,8 @@ $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oau
 /* If method is set change API call made. Test is called by default. */
 //$content = $connection->get('account/verify_credentials');
 $content = $connection->get('followers/ids', array('screen_name' => 'YaoYaoHacks'));
-//$DH = $connection->get('users/show', array('screen_name' => 'DoerHub');
-
+$DH = $connection->get('users/show', array('screen_name' => 'DoerHub'));
+$BHList = $connection->get('lists/list', array('screen_name' => 'DoerHub', 'user_id' => ($DH->id_string)));
 /*1431716216
 'lists/list'
 103277668
