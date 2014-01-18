34e67a37156d8c9b443c612a2fc4a850bdbee5d0
Jake <wizecracker55@yahoo.com>
2014-01-18 01:37:45 -0600

    a

--- Twitter/index.php
+++ Twitter/index.php
@@ -35,8 +35,9 @@ $Members =  $connection->get('lists/members', array('list_id' => $BHID));
 $followers = array();
 for($x = 0; $x <= count($Members->users); $x++)
 {
-	array_push($followers, $Members->users[$x]->screen_name => count($connection->get('followers/ids', array('screen_name' => $Members->users[$x]->screen_name))->ids));
+	array_push($followers, count($connection->get('followers/ids', array('screen_name' => $Members->users[$x]->screen_name))->ids));
 }
+//$Members->users[$x]->screen_name =>
 //$Members->users[$x]->name =>
 //$Members->users[$x]->name => 
 /*1431716216
