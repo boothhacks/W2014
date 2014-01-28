79ae943e148cdcdf22dcc216f4d3ee732084943f
Jake <wizecracker55@yahoo.com>
2014-01-28 03:20:30 -0600

    Add Searchme directory structure for week 3 challenge

--- Tools/prep_challenge_3.php
+++ Tools/prep_challenge_3.php
@@ -6,48 +6,54 @@ function generateRandomString($length = 10) {
     for ($i = 0; $i < $length; $i++) {
         $randomString .= $characters[rand(0, strlen($characters) - 1)];
     }
+    //echo $randomString;
     return $randomString;
 }
-function generateRandomFile($targetString, $targetName, $stringDivisor, $nameDivisor){
-	$fileName = generateRandomString(10);
+
+
+function generateRandomFile($filePath, $targetString, $targetName, $stringDivisor, $nameDivisor)
+{
+    $fileName = generateRandomString(10);
+    $fileString = generateRandomString(1000);
 	if(rand(0, $nameDivisor) == 0)
 	{
-		$filename = substr_replace($fileName, $targetName, rand(0, $fileName.length - $targetName.length));
+		$fileName = substr_replace($fileName, $targetName, rand(0, strlen($fileName) - strlen($targetName)), 0);
 	}
-	file_put_contents($filename, generateRandomString(10), FILE_APPEND);
+    if(rand(0, $stringDivisor) == 0)
+    {
+        $fileString = substr_replace($fileString, $targetString, rand(0, strlen($fileString) - strlen($targetString)), 0);
+    }
+	file_put_contents($filePath."/".$fileName.".txt", $fileString, 0);
+    //echo "FileName: ".$filename."\r\n";
+    return $fileName;
 }
+
 function createRandomFileStructure($filePath = "../Searchme", $maxDepth = 6, $maxBredth = 5, $maxFiles = 6, $targetString = "Job", $targetName="Apply", $stringDivisor = 10, $nameDivisor = 15) {
+    
+    mkdir($filePath);
+
+    echo $maxDepth;
     if($maxDepth <= 0)
     {
+        //echo "maxDepth below threshold";
     	return true;    	
     }
 
     $bredth = rand(0, $maxBredth);
     $files = rand(0, $maxFiles);
 
-    for($i = 0; $i < $files $i++)
+    for($i = 0; $i < $files; $i++)
     {
-		createRandomFile($targetString, $targetName, $stringDivisor, $nameDivisor);
+		generateRandomFile($filePath, $targetString, $targetName, $stringDivisor, $nameDivisor);
 	}
 
-    for($i = 0; i < $bredth; $i++)
+    for($i = 0; $i < $bredth; $i++)
     {
-		$depth = rand(1, $maxDepth - 1);
+		$depth = rand(0, $maxDepth - 1);
 		$folderName = generateRandomString(10);
-		if(rand(0, $nameDivisor) == 0)
-		{
-			$foldername = substr_replace($folderName, $targetName, rand(0, $folderName.length - $targetName.length));
-		}
-		createRandomFileStructure($filePath."/".$folderName, $maxBredth, $depth, $maxFiles, $targetString, $targetName, $stringDivisor, $nameDivisor);
-	}
+        //echo "foldername: ".$folderName."\n\r";
+		createRandomFileStructure($filePath."/".$folderName, $depth, $maxBredth, $maxFiles, $targetString, $targetName, $stringDivisor, $nameDivisor);  
+    }
 }
 
-$file = 'people.txt';
-// Open the file to get existing content
-$current = file_get_contents($file);
-
-// Append a new person to the file
-$current .= "John Smith\n";
-
-// Write the contents back to the file
-file_put_contents($file, $current);
+createRandomFileStructure("../Searchme", 10, 10, 10, "Job", "apply", 5, 5);
