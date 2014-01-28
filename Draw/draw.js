f3ba5d7fc35b7bd5aacaaed7a428e7214d1c81fe
Jake Walker <jakewalk@ucla.edu>
2014-01-27 23:28:13 -0600

    adding example logo

--- Draw/draw.js
+++ Draw/draw.js
@@ -1,12 +1,48 @@
+function drawShape(ctx, radians, granularity, divisor, scalar, xoffset, yoffset)
+{
+
+	var pointsx = new Array();
+	var pointsy = new Array();
+
+    for(var theta = 0; theta < radians; theta += granularity)
+	{
+		//r is the radius in polar coordinates
+		var r = 100 * Math.sin(scalar/divisor*theta);
+
+		//convert to cartesian coordinates, add an offset so we can show multiple graphs on one screen
+		var x = xoffset + r * Math.cos(theta);
+		var y = yoffset + r * Math.sin(theta);
+
+		//push the values onto the back of the arrays
+		pointsx.push(x);
+		pointsy.push(y);
+	}
+	/*
+	for(var k = 1; k < pointsx.length; k++)
+	{
+		//if you wanted to draw a background, you could do it here
+	}
+	*/
+
+	for(var k = 1; k < pointsx.length; k++)
+	{
+		ctx.beginPath();
+		ctx.moveTo(pointsx[k-1],pointsy[k-1]);
+		ctx.lineTo(pointsx[k],pointsy[k]);
+		ctx.stroke();			
+	}
+	return true;
+	//Save your file to a .png file- http://stackoverflow.com/questions/923885/capture-html-canvas-as-gif-jpg-png-pdf
+}
+
 function draw()
 {
 	//see reference at http://www.w3schools.com/tags/ref_canvas.asp
 	var canvas = document.getElementById("myCanvas");
 	canvas.setAttribute("width", window.innerWidth);
 	canvas.setAttribute("height", window.innerHeight);
-	canvas.setAttribute("style", "position: absolute; x:0; y:0;");
 
-	var width = 1
+	var width = 1;
 	var granularity = parseFloat(document.getElementById("granularity").value);
 	var radians = parseFloat(document.getElementById("radians").value);	
 	var divisor = parseFloat(document.getElementById("divisor").value);
@@ -14,37 +50,31 @@ function draw()
 	var ctx = canvas.getContext("2d");
 	ctx.strokeStyle=document.getElementById("color").value;
 	ctx.lineWidth = parseFloat(document.getElementById("width").value);
-	//things to try: shadowColor, shadowBlur, shadowOffsetX, shadowOffsetY, 
+	//things to try: shadowColor, shadowBlur, createLinearGradient, lineCap, arc, etc.
+	
 	for(var i = 1; i < 13; i++)
 	{
-		var pointsx = new Array();
-		var pointsy = new Array();
-	    for(var theta = 0; theta < radians; theta += granularity)
-		{
-			var r = 100 * Math.sin(i/divisor*theta);
-			var x = 100 + 200 * ((i-1)%4) + r * Math.cos(theta);
-			var y = 100 + 200 * Math.floor((i-1)/4) + r * Math.sin(theta);
-			pointsx.push(x);
-			pointsy.push(y);
-		}
-		/*
-		for(var k = 1; k < pointsx.length; k++)
-		{
-			//if you wanted to draw a background, you could do it here
-		}
-		*/
-		for(var k = 1; k < pointsx.length; k++)
-		{
-			ctx.beginPath();
-			ctx.moveTo(pointsx[k-1],pointsy[k-1]);
-			ctx.lineTo(pointsx[k],pointsy[k]);
-			ctx.stroke();			
-		}
+		var xoffset = 100 + 200 * ((i-1)%4);
+		var yoffset = 100 + 200 * Math.floor((i-1)/4);
+		drawShape(ctx, radians, granularity, divisor, i, xoffset, yoffset);
 	}
-	//Save your file to a .png file- http://stackoverflow.com/questions/923885/capture-html-canvas-as-gif-jpg-png-pdf
 }
-
-function drawtext()
+function drawWord(ctx, text, font, x, y, gradWidth, gradHeight, color1, color2)
+{
+	//see http://www.w3schools.com/tags/canvas_createlineargradient.asp
+	var grd=ctx.createLinearGradient(x, 0, x + gradWidth, gradHeight);
+	
+	//see http://www.w3schools.com/tags/canvas_addcolorstop.asp
+	grd.addColorStop(0,color1);
+	grd.addColorStop(1,color2);
+	
+	
+	ctx.fillStyle=grd;
+	ctx.font = font;
+	ctx.fillText(text, x, y);
+	ctx.strokeText(text, x, y);
+}
+function drawText()
 {
 	//see reference at http://www.w3schools.com/tags/ref_canvas.asp
 	var canvas = document.getElementById("myCanvas");
@@ -72,22 +102,6 @@ function drawtext()
 	
 	var ctx = canvas.getContext("2d");
 	
-	//see http://www.w3schools.com/tags/canvas_createlineargradient.asp
-	var grd1=ctx.createLinearGradient(text1x, 0, text1x + 200 ,0);
-	var grd2=ctx.createLinearGradient(text2x, 0, text2x + 200 ,0);
-	
-	//see http://www.w3schools.com/tags/canvas_addcolorstop.asp
-	grd1.addColorStop(0,text1color1);
-	grd1.addColorStop(1,text1color2);
-	
-	grd2.addColorStop(0,text2color1);
-	grd2.addColorStop(1,text2color2);
-	
-	ctx.fillStyle=grd1;
-	ctx.font = text1font;
-	ctx.fillText(text1, text1x, text1y);
-
-	ctx.fillStyle=grd2;
-	ctx.font = text2font;
-	ctx.fillText(text2, text2x, text2y);
+	drawWord(ctx, text1, text1font, text1x, text1y, 200, 0, text1color1, text1color2);
+	drawWord(ctx, text2, text2font, text2x, text2y, 200, 0, text2color1, text2color2);
 }
